7e285fb6455427181bd1456fef49983202b3e965
Danimo2109 <danimodanimo99@gmail.com>
2022-06-11 22:45:18 +0200

    Ein Fehler wurde behoben. Nicht alle unteren Noten wurden angezeigt.

--- js/action_cards.js
+++ js/action_cards.js
@@ -1,7 +1,15 @@
 Notenliste=["C1","D1","E1","F1","G1","A1","H1","Cis","Dis","Fis","Gis","Ais","D1b","E1b","G1b","A1b","H1b"];
 Länge=Notenliste.length -1;
 
-function Random_buttons(params) {
+function noteNameFromFileName(bildDateiName) {
+    let lastSlashPosition =bildDateiName.lastIndexOf("/") //Wir brauchen den letzten Slash, weil danach der Notenname kommt. 
+    notenName=bildDateiName.substr (lastSlashPosition + 1,3).replace (".", ""); // Aus der Bilddatei wird der Notenname rausgeschnitten. Und zwar ab der ersten Position nach dem Slash (deswegen +1). 
+                                                                            // Der Notenname kann max. 3 Zeichen lang sein. Sollte der Name jedoch nur 2 Zeichen lang sein, würde danach ein Punkt(von ".png") kommen, da
+                                                                            // der Punkt in dem Fall das dritte Zeichen nach dem Slash wäre und er deshalb durch nichts ersetzt wird.
+    return notenName
+}
+ 
+function Random_buttons(params) {// In dieser Funktion werden die unteren Notennamen generiert
     for (let index = 0; index < Knöpfe.length/*28*/; index++) { // Diese Schleife läuft über die Knöpfe unten
         const Knopf = Knöpfe[index]; // Knopf, bei dem die Schleife gerade ist
         Knopf.innerHTML=Notenliste[Math.round(Math.random()*Länge)]; // Wir nehmen eine zufällige Note aus der Notenliste und packen sie in den Knopf       
@@ -14,12 +22,12 @@ function Random_buttons(params) {
         
         imgId= "card" + notenNummer + "img"; // Die imgId der Note wird aus card + Notennummer + img zusammengesetzt
         Img = document.getElementById (imgId) // Img wird genommen
-        bildDatei= Img.src; // Die Bilddatei wird genommen
-        notenName=bildDatei.substr (113,3).replace (".", ""); // Aus der Bilddatei wird der Notenname rausgeschnitten
+        bildDateiName= Img.src; // Die Bilddatei wird genommen
+        notenName=noteNameFromFileName(bildDateiName)
         const Knopf = Knöpfe[Math.trunc(notenNummer*Knöpfe.length/Notenliste.length)+Math.trunc(Math.random()*Knöpfe.length/Notenliste.length)] // Wir nehmen einen zufälligen Knopf
         Knopf.innerHTML=notenName;  // Wir nehmen den Notennamen und setzten ihn in den Knopf      
     }
-      
+
 }
 
 
@@ -103,7 +111,7 @@ function notePressed(p) {
     imgId= "card" + markedNote + "img";
     markedImg = document.getElementById (imgId)
     src= markedImg.src;
-    markedNoteName=src.substr (113,3); 
+    markedNoteName=noteNameFromFileName(src); 
     markedNote = markedNote + 1;// Die Markierung geht auf die nächste Note
     if (markedNote == 5)// Sobald es keine Note mehr gibt, die markiert werden kann, dann wird wieder die Note null markiert und alles wiederholt sich
        markedNote = 0// Die Markierung überträgt sich auf die erste Note
@@ -117,7 +125,7 @@ function notePressed(p) {
     }
 
 pressedNoteName=p.target.innerText
-if (markedNoteName.replace (".", "")==pressedNoteName){// Es wird überprüft, ob die richtige Note gedrückt wurde.
+if (markedNoteName==pressedNoteName){// Es wird überprüft, ob die richtige Note gedrückt wurde.
     score ++ //Wenn die richtige Note gedrückt wurde, bekommt man einen Punkt.
 }
 else{
